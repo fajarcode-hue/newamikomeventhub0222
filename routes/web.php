7da872c74146be6832a15b7f3efa5c2b9dcf50d9
fajarcode-hue <fajarnaga2005@gmail.com>
2026-05-08 09:20:53 +0700

    Pertemuan 6 Crud Laravel

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,33 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\EventController as EventAdminController;
 
-Route::get('/', function () {
-    return view('index');
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::resource('events', EventAdminController::class);
 });
 
-Route::get('/detail', function () {
-    return view('event_detail');
-});
 
-Route::get('/checkout', function () {
-    return view('checkout');
-});
+// Route::get('/', function () {
+//     return view('index');
+// });
 
-Route::get('/admin', function () {
-    return view('admin.dashboard');
-});
+// Route::get('/detail', function () {
+//     return view('event_detail');
+// });
 
-Route::get('/adminkelola', function () {
-    return view('admin.event');
-});
+// Route::get('/checkout', function () {
+//     return view('checkout');
+// });
 
-Route::get('/adminlaporan', function () {
-    return view('admin.transaction');
-});
+// Route::get('/admin', function () {
+//     return view('admin.dashboard');
+// });
+
+// Route::get('/adminkelola', function () {
+//     return view('admin.event');
+// });
+
+// Route::get('/adminlaporan', function () {
+//     return view('admin.transaction');
+// });
